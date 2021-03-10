34d72d3e59009ef33cc03b9eec6483e4e6acf446
cepps <cepps@regionalmanagement.com>
2021-03-10 15:35:50 -0500

    Create your_own_list.py

---
+++ your_own_list.py
@@ -0,0 +1,10 @@
+autos = ['Dodge Challenger', 'Ford GT', 'GMC Typhoon']
+
+message = "I would love to own a/an " + autos[0] + "."
+print(message)
+
+message = "I would love to own a/an " + autos[1] + "."
+print(message)
+
+message = "I would love to own a/an " + autos[2] + "."
+print(message)
