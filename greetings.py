7c43db36cd85fc1cc65c513cc9b27231a21fc360
cepps <cepps@regionalmanagement.com>
2021-03-10 15:35:41 -0500

    Create greetings.py

---
+++ greetings.py
@@ -0,0 +1,10 @@
+names = ['josh transou', 'elaina warren', 'matt earnest']
+
+message = "Hello, " + names[0].title() + ". Would you like to play a game?"
+print(message)
+
+message = "Hello, " + names[1].title() + ". Would you like to play a game?"
+print(message)
+
+message = "Hello, " + names[2].title() + ". Would you like to play a game?"
+print(message)
