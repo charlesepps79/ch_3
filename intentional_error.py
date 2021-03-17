04e77764dafa47aca81bed47d34963dbfd1d36f1
cepps <cepps@regionalmanagement.com>
2021-03-17 16:30:09 -0400

    Create intentional_error.py

---
+++ intentional_error.py
@@ -0,0 +1,9 @@
+# 3-11. Intentional Error: If you haven’t received an index error in 
+# one of your programs yet, try to make one happen. Change an index in 
+# one of your programs to produce an index error. Make sure you correct 
+# the error before closing the program.
+
+movies = ['the avengers', 'logan', 'guardians of the galaxy', 
+          'iron man', 'avengers endgame']
+del movies[5]
+print(movies)
